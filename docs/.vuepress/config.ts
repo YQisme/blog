c429e5946f1f855391a1fc9b848ed0a7702ab9e0
YQisme <1398757912@qq.com>
2022-02-03 11:55:13 +0800

    test

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -68,7 +68,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         text: '实践',
         link: '/practice/',
         items: [
-          { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+          { text: '动手学深度学习', link: '/pages/baed3f/' },
           { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
